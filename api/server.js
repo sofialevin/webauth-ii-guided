0ea44903fe9a83e947ff3a9633b918df5198e4a9
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-17 10:13:54 -0800

    Refacted middleware to use session.

--- api/server.js
+++ api/server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const sessions = require('express-session');
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfiguration = {
+  name: 'chocolatechip',
+  secret: 'keep it secret, keep it safe',
+  saveUninitialized: true,
+  resave: false,
+  cookie: {
+    maxAge: 1000 * 60 * 10,
+    secure: false,
+    httpOnly: true
+  }
+}
+
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(sessions(sessionConfiguration));
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
